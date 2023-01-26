9a741381a7a398558113b6a437447c51e8ed6dce
Rahn20 <ranimhanna@hotmail.com>
2023-01-26 02:02:00 +0000

    Removed get customers/logs and added rent/return methods

--- simulation.py
+++ simulation.py
@@ -5,6 +5,7 @@ Mainprogram for scooter simulation
 """
 
 
+from datetime import datetime
 import time
 from src.scooter import Scooter
 from src.api import ApiData
@@ -18,14 +19,15 @@ class Simulation():
     # saves scooter id in an array
     _scooter_id = []
 
-    # saves log id in an array
-    _log_id = []
+    # saves station in an array
+    _station = []
 
-    # saves station id in an array
-    _station_id = []
+    # saves user id in an array
+    _user_id = []
 
     # saves city info in an array
     _city_data = []
+
 
     def __init__(self):
         """ Initialize class """
@@ -35,7 +37,7 @@ class Simulation():
 
 
     def move(self) -> None:
-        """ Move scooters around the cities. Simulation time is 2 minutes. """
+        """ Moves scooters around the cities. Simulation time is 2 minutes. """
         end_time = time.time() + 120    # 2 minutes
 
         print("\nStep 2 - Moving scooters . . . . . . . .")
@@ -48,17 +50,26 @@ class Simulation():
 
                 self.scooter.add_scooter_to_dict(data)
                 self.scooter.add_city_to_dict(self._city_data[count])
-                self.scooter.set_station_id(self._station_id[count])
-                
+
                 count = self.run(count)
 
                 count += 1
-                time.sleep(0.003)
+                time.sleep(0.001)
 
+
+    def delete_and_remove(self, count) -> None:
+        """ Removes a specific element from the list and stop the scooter """
+        self.end(count)
+
+        del self._scooter_id[count]
+        del self._station[count]
+        del self._user_id[count]
+        del self._city_data[count]
+    
 
     def run(self, count: int) -> int:
         """
-        Check if the scooter is inside the city and check battery level, if TRUE
+        Checks if the scooter is inside the city and check battery level, if TRUE
         the scooter will be returned otherwise the scooter will continue moving.
         """
         id = self._scooter_id[count]
@@ -67,44 +78,31 @@ class Simulation():
             print("\nScooter {} is outside of the city\n".format(id))
             print("Scooter {} will be returned.".format(id))
 
-            self.end(count)
-
-            del self._scooter_id[count]
-            del self._log_id[count]
-            del self._station_id[count]
-            del self._city_data[count]
-
+            self.delete_and_remove()
             count -= 1
         elif self.scooter.check_battery():
             print("\n\033[1;31m*\033[1;0m Low battery!! the scooter {} needs to be charged.".format(id))
             print("Scooter {} will be returned.".format(id))
 
-            self.end(count)
-
-            del self._scooter_id[count]
-            del self._log_id[count]
-            del self._station_id[count]
-            del self._city_data[count]
-
+            self.delete_and_remove()
             count -= 1
         else:
             self.scooter.change_location()
             self.scooter.move_scooter()
-            self.api.update_scooter()
+            self.api.update_rented_scooter()
 
         return count
+
 
     def end(self, count):
         """
         Scooter will be checked if it's outside the city/has low battery level or need
-        meantience. The scooter will be returned and the log will be uppdated.
+        meantience. The scooter will be returned and the log/payment/account will be uppdated.
         """
+        self.api.station = self._station[count]
+        self.handler.api = ApiData(self._user_id[count])
+
         self.handler.end_rental()
-        self.api.update_scooter()
-
-        self.api.log_id = self._log_id[count]
-        self.api.update_log()
-
 
 
     def return_scooters(self):
@@ -119,11 +117,10 @@ class Simulation():
 
             self.scooter.add_scooter_to_dict(data)
             self.scooter.add_city_to_dict(self._city_data[count])
-            self.scooter.set_station_id(self._station_id[count])
             self.end(count)
 
             count += 1
-            time.sleep(0.003)
+            time.sleep(0.001)
 
 
     def main(self, total:int):
@@ -132,10 +129,12 @@ class Simulation():
         print("\nThe simulation takes around 3 minutes, do not try to break/stop the program.\n")
         print("\nStep 1 - Renting scooters . . . . . . . .")
 
-        all_customers = self.api.get_all_customers()
-
+        current = datetime.now().time().strftime("%H:%M:%S")
+        self.handler.start_time = [int(current[:2]), int(current[3:5]), int(current[6:8])]
         count = 1
-        customer = 0
+
+        # user id 6 means customer id 1, start counting from 6
+        user = 6 
 
         while count <= total:
             data = self.api.get_scooter_data(scooter_id = count)
@@ -143,21 +142,17 @@ class Simulation():
             if self.scooter.check_scooter_status(data):
                 # get city
                 city = self.api.get_city_data()
-                self.scooter.add_city_to_dict(city)
                 self._city_data.append(city)
                 self._scooter_id.append(count)
 
-                # create log
-                self.api.user_id = str(all_customers[customer]["id"])
-                self.api.create_log()
-                self._log_id.append(self.api.log_id)
+                # create log/payment and update customer's account
+                self._user_id.append(user)
+                self.api.user_id = str(user)
 
-                # get station
-                zone = self.scooter.get_zone_id()
-                station = self.api.get_station(zone)
-                self._station_id.append(station)
+                self.api.rent_scooter()
+                self._station.append(self.api.station)
 
-                customer += 1
+                user += 1
             else:
                 print("\n\033[1;31m*\033[1;0m Scooter {} is not available.\n".format(count))
 
